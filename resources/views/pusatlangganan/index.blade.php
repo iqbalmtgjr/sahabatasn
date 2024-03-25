b0bd25ce256c488286c7cea99eb9acb6951fcfda
supri1013 <yatnok97@gmail.com>
2024-03-25 23:09:48 +0700

    malam_selsa3

--- resources/views/pusatlangganan/index.blade.php
+++ resources/views/pusatlangganan/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <!--begin::Content wrapper-->
 <div class="d-flex flex-column flex-column-fluid">
-							<!--begin::Toolbar-->
-							<div id="kt_app_toolbar" class="app-toolbar pt-7 pt-lg-10">
+	<!--begin::Toolbar-->
+	<div id="kt_app_toolbar" class="app-toolbar pt-7 pt-lg-10">
 								<!--begin::Toolbar container-->
 								<div id="kt_app_toolbar_container" class="app-container container-fluid d-flex align-items-stretch">
 									<!--begin::Toolbar wrapper-->
@@ -28,10 +28,90 @@
 									<!--end::Toolbar wrapper-->
 								</div>
 								<!--end::Toolbar container-->
-							</div>
-							<!--end::Toolbar-->
-							<!--begin::Content-->
-							<div id="kt_app_content" class="app-content flex-column-fluid">
+	</div>
+	<!--end::Toolbar-->
+	<!--begin::Content-->
+		<div class="row g-10">
+													<!--begin::Col-->
+			<div class="col-xl-4">
+														<div class="d-flex h-100 align-items-center">
+															<!--begin::Option-->
+															<div class="w-100 d-flex flex-column flex-center rounded-3 bg-light bg-opacity-75 py-15 px-10">
+																<!--begin::Heading-->
+																<div class="mb-7 text-center">
+																	<!--begin::Title-->
+																	<h1 class="text-dark mb-5 fw-bolder">Startup</h1>
+																	<!--end::Title-->
+																	<!--begin::Description-->
+																	<div class="text-gray-600 fw-semibold mb-5">Optimal for 10+ team size
+																	<br>and new startup</div>
+																	<!--end::Description-->
+																	<!--begin::Price-->
+																	<div class="text-center">
+																		<span class="mb-2 text-primary">$</span>
+																		<span class="fs-3x fw-bold text-primary" data-kt-plan-price-month="39" data-kt-plan-price-annual="399">39</span>
+																		<span class="fs-7 fw-semibold opacity-50">/
+																		<span data-kt-element="period">Mon</span></span>
+																	</div>
+																	<!--end::Price-->
+																</div>
+																<!--end::Heading-->
+																<!--begin::Features-->
+																<div class="w-100 mb-10">
+																	<!--begin::Item-->
+																	<div class="d-flex align-items-center mb-5">
+																		<span class="fw-semibold fs-6 text-gray-800 flex-grow-1 pe-3">Up to 10 Active Users</span>
+																		<i class="ki-outline ki-check-circle fs-1 text-success"></i>
+																	</div>
+																	<!--end::Item-->
+																	<!--begin::Item-->
+																	<div class="d-flex align-items-center mb-5">
+																		<span class="fw-semibold fs-6 text-gray-800 flex-grow-1 pe-3">Up to 30 Project Integrations</span>
+																		<i class="ki-outline ki-check-circle fs-1 text-success"></i>
+																	</div>
+																	<!--end::Item-->
+																	<!--begin::Item-->
+																	<div class="d-flex align-items-center mb-5">
+																		<span class="fw-semibold fs-6 text-gray-800 flex-grow-1 pe-3">Analytics Module</span>
+																		<i class="ki-outline ki-check-circle fs-1 text-success"></i>
+																	</div>
+																	<!--end::Item-->
+																	<!--begin::Item-->
+																	<div class="d-flex align-items-center mb-5">
+																		<span class="fw-semibold fs-6 text-gray-600 flex-grow-1">Finance Module</span>
+																		<i class="ki-outline ki-cross-circle fs-1"></i>
+																	</div>
+																	<!--end::Item-->
+																	<!--begin::Item-->
+																	<div class="d-flex align-items-center mb-5">
+																		<span class="fw-semibold fs-6 text-gray-600 flex-grow-1">Accounting Module</span>
+																		<i class="ki-outline ki-cross-circle fs-1"></i>
+																	</div>
+																	<!--end::Item-->
+																	<!--begin::Item-->
+																	<div class="d-flex align-items-center mb-5">
+																		<span class="fw-semibold fs-6 text-gray-600 flex-grow-1">Network Platform</span>
+																		<i class="ki-outline ki-cross-circle fs-1"></i>
+																	</div>
+																	<!--end::Item-->
+																	<!--begin::Item-->
+																	<div class="d-flex align-items-center">
+																		<span class="fw-semibold fs-6 text-gray-600 flex-grow-1">Unlimited Cloud Space</span>
+																		<i class="ki-outline ki-cross-circle fs-1"></i>
+																	</div>
+																	<!--end::Item-->
+																</div>
+																<!--end::Features-->
+																<!--begin::Select-->
+																<a href="#" class="btn btn-sm btn-primary">Select</a>
+																<!--end::Select-->
+															</div>
+															<!--end::Option-->
+														</div>
+			</div>
+													<!--end::Col-->
+		</div>
+	{{-- <div id="kt_app_content" class="app-content flex-column-fluid">
 								<!--begin::Content container-->
 								<div id="kt_app_content_container" class="app-container container-fluid">
 									<!--begin::Post card-->
@@ -98,8 +178,8 @@
 									<!--end::Post card-->
 								</div>
 								<!--end::Content container-->
-							</div>
-							<!--end::Content-->
-						</div>
-						<!--end::Content wrapper-->
+	</div> --}}
+	<!--end::Content-->
+</div>
+<!--end::Content wrapper-->
 @endsection
