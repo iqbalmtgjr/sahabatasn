039953e9122707956bc172f13cc7d75d491c8e83
Muhammad Iqbal <iqbalmtgjr@gmail.com>
2024-03-25 22:18:54 +0700

    ubah checkrole

--- app/Http/Middleware/checkRole.php
+++ app/Http/Middleware/checkRole.php
@@ -18,33 +18,11 @@ class checkRole
      */
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        if (!($request->user() && $this->checkRole($request->user(), $roles))) {
-            toastr()->error('Anda tidak memiliki hak akses', 'Gagal');
-            return redirect()->back();
+        if (in_array($request->user()->role, $roles)) {
+            return $next($request);
         }
 
-        return $next($request);
-    }
-
-    /**
-     * Check if user has at least one of the given roles.
-     *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable|null  $user
-     * @param  array  $roles
-     * @return bool
-     */
-    protected function checkRole($user, array $roles): bool
-    {
-        if (!$user) {
-            return false;
-        }
-
-        foreach ($roles as $role) {
-            if ($user->hasRole($role)) {
-                return true;
-            }
-        }
-
-        return false;
+        toastr()->error('Anda tidak memiliki hak akses', 'Gagal');
+        return redirect()->back();
     }
 }
